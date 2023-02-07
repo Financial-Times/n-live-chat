fffe5fd04aa8c193618788b877db299894851029
danieleruiz <daniel.ruiz@ft.com>
2023-02-07 13:17:04 +0100

    ACC-2127: fix url in client side

--- src/js/browser.ts
+++ src/js/browser.ts
@@ -79,7 +79,9 @@ class LiveChat {
 						}
 						this.button.onclick = () => {
 							if (chatterBox) {
-								const baseUrl: string = this.flags.get('liveChatStaging') ? process.env.LIVE_CHAT_STAGING_HOST : process.env.LIVE_CHAT_PROD_HOST;
+								const LIVE_CHAT_STAGING_HOST = 'https://ip-chatterbox-client-staging.herokuapp.com';
+								const LIVE_CHAT_PROD_HOST = 'https://d.la1-c1cs-iad.salesforceliveagent.com'
+								const baseUrl: string = this.flags.get('liveChatStaging') ? LIVE_CHAT_STAGING_HOST : LIVE_CHAT_PROD_HOST;
 								const url: string = `${baseUrl}${this.config.buttonReference}/${this.config.deploymentId}`;
 								window.open(url, 'FT Live Chat', 'height=474px, width=467px')
 							} else {
